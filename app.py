1bedf74b28d5b25a29901fa67230c3bf4c20ecb6
qordbstj <111742995+qordbstj@users.noreply.github.com>
2022-08-22 20:08:07 +0900

    app

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ num3 = len(df[df['설립구분'] == '사립']['학교'].unique())
 col1, col2, col3 = st.columns(3)
 col1.metric(label="대학수", value=num1, delta="")
 col2.metric(label="국공립학교", value=num2, delta=f'{round(num2/num1 * 100, 2)}%')
-col3.metric(label="사립학교", value=num3, delta="1.2 °F")
+col3.metric(label="사립학교", value=num3, delta=f'{round(num3/num1 * 100, 2)}%')
 
 st.write('기숙사현황')
