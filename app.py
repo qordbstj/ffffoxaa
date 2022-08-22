4c03510324790e7f2794468f11f768d9e5862b2e
qordbstj <111742995+qordbstj@users.noreply.github.com>
2022-08-22 19:50:31 +0900

    app

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ import numpy as np
 data = pd.read_csv('기숙사수용현황분석.csv')
 st.dataframe(data)
 
+st.metric(label="대학수", value="70 °F", delta="1.2 °F")
+st.metric(label="국공립학교", value="70 °F", delta="1.2 °F")
+st.metric(label="사립학교", value="70 °F", delta="1.2 °F")
+
 st.write('기숙사현황')
