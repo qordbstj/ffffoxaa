2cd69a06a1cdc8031dfb203d8da9c394823d5e6b
qordbstj <111742995+qordbstj@users.noreply.github.com>
2022-08-22 20:00:12 +0900

    app

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ import numpy as np
 
 data = pd.read_csv('기숙사수용현황분석.csv')
 st.dataframe(data)
+df = data[data['학교종류'] == '대학교']
+num1 = len(df['학교'].unique())
+num2 = len(df[df['설립구분'] != '사립']['학교'].unique())
+num3 = len(df[df['설립구분'] == '사립']['학교'].unique())
 
-st.metric(label="대학수", value="70 °F", delta="1.2 °F")
-st.metric(label="국공립학교", value="70 °F", delta="1.2 °F")
-st.metric(label="사립학교", value="70 °F", delta="1.2 °F")
+col1, col2, col3 = st.columns(3)
+st.metric(label="대학수", value=num1, delta="1.2 °F")
+st.metric(label="국공립학교", value=num2, delta="1.2 °F")
+st.metric(label="사립학교", value=num3, delta="1.2 °F")
 
 st.write('기숙사현황')
